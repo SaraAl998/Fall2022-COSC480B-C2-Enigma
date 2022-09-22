463c7cca97ab3092816a79495fd85e05fbc8ab07
sjinseob <sjinseob@gmail.com>
2022-09-22 10:21:28 -0400

    testing + comments

--- training.py
+++ training.py
@@ -1,11 +1,13 @@
 import json
 
-f = open("training.txt", 'r')
+f = open("training_all_words.txt", 'r')
 s = f.read()
 lfreq = {}
+
 for i in range(len(s)-1):
     l1 = s[i].lower()
     l2 = s[i+1].lower()
+
     if (l1.isalpha() and l2.isalpha()):
         if l1 not in lfreq:
             lfreq[l1] = {}
@@ -14,6 +16,7 @@ for i in range(len(s)-1):
             lfreq[l1][l2] = 0
         lfreq[l1][l2] += 1
         lfreq[l1]['tot'] += 1
+
 out = open("trained.txt", "w")
 lfreq = json.dumps(lfreq)
 out.write(lfreq)
